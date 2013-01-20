d5d3ad998cf1dcd8ffc16bce490920089ce3291c
Chris Kettenbach <kettenbach@gmail.com>
2013-01-20 07:30:03 -0800

    redis credentials

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ var app = express();
 var RedisStore = require('connect-redis')(express);
 
 app.configure(function(){
-  var redisUrl = url.parse(process.env.REDISTOGO_URL);
-    //redisAuth = redisUrl.auth.split(':');
+  var redisUrl = url.parse(process.env.REDISTOGO_URL),
+    redisAuth = redisUrl.auth.split(':');
   app.set('redisHost', redisUrl.hostname);
   app.set('redisPort', redisUrl.port);
-  //app.set('redisDb', redisAuth[0]);
-  //app.set('redisPass', redisAuth[1]);
+  app.set('redisDb', redisAuth[0]);
+  app.set('redisPass', redisAuth[1]);
 });
 
 app.configure(function(){
@@ -42,9 +42,9 @@ app.configure(function(){
     secret: 'my super sekret hash',
     store: new RedisStore({
       host: app.get('redisHost'),
-      port: app.get('redisPort')
-      //db: app.get('redisDb'),
-      //pass: app.get('redisPass')
+      port: app.get('redisPort'),
+      db: app.get('redisDb'),
+      pass: app.get('redisPass')
     })
   }));
   app.use(app.router);
